0aeddc32fe6074c16946243ec224f6fbe59ad81e
MarVikt <v.m.podporogie@gmail.com>
2022-04-06 16:17:01 +0300

    completed task #09

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const title = document.getElementsByTagName('h1')[0];
+const buttons = document.getElementsByClassName('handler_btn');
+const buttonPlus = document.querySelector('.screen-btn');
+const otherItems = document.querySelectorAll('.other-items');
+const otherPercent = document.querySelectorAll('.other-items.percent');
+const otherNumber = document.querySelectorAll('.other-items.number');
+const rollback = document.querySelector('.rollback input[type="range"]');
+const rangeValue = document.querySelector('.rollback span.range-value');
+const totalInputs = document.getElementsByClassName('total-input');
+let screens = document.querySelectorAll('.screen');
+
+// console.log(screens);
+
+
+
